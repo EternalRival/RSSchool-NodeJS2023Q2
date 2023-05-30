23ababf1846548428841c69b00b8d5e4e0831a87
EternalRival <eternalrival8@gmail.com>
2023-05-30 05:40:27 +0300

    feat(fs): implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,30 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const TASK_OBJECTIVE = {
+    source: {
+      dirName: 'files',
+      fileName: 'fileToRemove.txt',
+    },
+    errors: {
+      doesNotExists: { code: 'ENOENT', message: 'FS operation failed' },
+    },
+  };
+
+  const helpers = {
+    __dirname: dirname(fileURLToPath(import.meta.url)),
+    handleError(error, objectiveError) {
+      if (error.code === objectiveError?.code) Object.assign(error, objectiveError);
+      throw error;
+    },
+  };
+
+  const filePath = resolve(helpers.__dirname, TASK_OBJECTIVE.source.dirName, TASK_OBJECTIVE.source.fileName);
+  return rm(filePath).catch((error) => {
+    return helpers.handleError(error, TASK_OBJECTIVE.errors.doesNotExists);
+  });
 };
 
 await remove();
