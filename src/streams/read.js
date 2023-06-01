3d4dee3d22657f362e18dd1fba0dd564e5af7e39
EternalRival <eternalrival8@gmail.com>
2023-06-01 23:57:05 +0300

    feat(streams): implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const TASK_OBJECTIVE = {
+    source: {
+      dirName: 'files',
+      fileName: 'fileToRead.txt',
+    },
+  };
+  const helpers = {
+    __dirname: dirname(fileURLToPath(import.meta.url)),
+  };
+
+  const filePath = resolve(helpers.__dirname, TASK_OBJECTIVE.source.dirName, TASK_OBJECTIVE.source.fileName);
+  const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+  readStream.pipe(process.stdout);
 };
 
 await read();
