a3a7d3fb0a20228ce64e975f28ed715d53d34c0d
EternalRival <eternalrival8@gmail.com>
2023-06-02 03:46:34 +0300

    feat(streams): implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+  const TASK_OBJECTIVE = {
+    destination: {
+      dirName: 'files',
+      fileName: 'fileToWrite.txt',
+    },
+  };
+  const helpers = {
+    __dirname: dirname(fileURLToPath(import.meta.url)),
+  };
+
+  const filePath = resolve(helpers.__dirname, TASK_OBJECTIVE.destination.dirName, TASK_OBJECTIVE.destination.fileName);
+  const writeStream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+  process.stdin.pipe(writeStream);
 };
 
 await write();
