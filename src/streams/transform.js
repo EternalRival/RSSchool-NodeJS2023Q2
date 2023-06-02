8e6afeb2334916d5ace60bcff3387b635a8ab485
EternalRival <eternalrival8@gmail.com>
2023-06-02 04:43:53 +0300

    feat(streams): implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
+import { Transform } from 'stream';
+import { EOL } from 'os';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform: (chunk, _, callback) => callback(null, `${chunk.reverse().slice(1)}${EOL}`),
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
