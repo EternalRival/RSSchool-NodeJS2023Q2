d789a8520133fec70659a95613c364fe7fda49d6
EternalRival <eternalrival8@gmail.com>
2023-06-19 14:48:02 +0300

    fix: replace buffer reversing with array reversing to fix cyrillic issue

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,7 +3,7 @@ import { EOL } from 'os';
 
 const transform = async () => {
   const transformStream = new Transform({
-    transform: (chunk, _, callback) => callback(null, `${chunk.reverse().slice(1)}${EOL}`),
+    transform: (chunk, _, callback) => callback(null, [...`${chunk}`.slice(0, -1)].reverse().join('') + EOL),
   });
 
   process.stdin.pipe(transformStream).pipe(process.stdout);
