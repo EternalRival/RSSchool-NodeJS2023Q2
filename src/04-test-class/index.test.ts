deb09f75af09160b8375efd56072d635f4a3dab3
EternalRival <eternalrival8@gmail.com>
2023-07-02 16:03:44 +0300

    test(04): implement test-class

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,116 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  BankAccount,
+  InsufficientFundsError,
+  SynchronizationFailedError,
+  TransferFailedError,
+  getBankAccount,
+} from '.';
+
+import lodash from 'lodash';
 
 describe('BankAccount', () => {
+  const initialBalance = 9000;
+  let mainAccount: BankAccount;
+  let secondAccount: BankAccount;
+  let operationAmount: number;
+
+  beforeEach(() => {
+    mainAccount = getBankAccount(initialBalance);
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(mainAccount.getBalance()).toBe(initialBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    operationAmount = initialBalance * 2;
+
+    expect(() => mainAccount.withdraw(operationAmount)).toThrowError(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    secondAccount = getBankAccount(initialBalance);
+    operationAmount = initialBalance * 2;
+
+    expect(() =>
+      mainAccount.transfer(operationAmount, secondAccount),
+    ).toThrowError(InsufficientFundsError);
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    operationAmount = initialBalance * 0.5;
+
+    expect(() =>
+      mainAccount.transfer(operationAmount, mainAccount),
+    ).toThrowError(TransferFailedError);
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    operationAmount = initialBalance * 0.5;
+    const expectedBalance = initialBalance + operationAmount;
+
+    mainAccount.deposit(operationAmount);
+    expect(mainAccount.getBalance()).toBe(expectedBalance);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    operationAmount = initialBalance * 0.5;
+    const expectedBalance = initialBalance - operationAmount;
+
+    mainAccount.withdraw(operationAmount);
+    expect(mainAccount.getBalance()).toBe(expectedBalance);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    secondAccount = getBankAccount(initialBalance);
+    operationAmount = initialBalance * 0.5;
+
+    const expectedMainBalance = initialBalance - operationAmount;
+    const expectedSecondBalance = initialBalance + operationAmount;
+
+    mainAccount.transfer(operationAmount, secondAccount);
+
+    expect(mainAccount.getBalance()).toBe(expectedMainBalance);
+    expect(secondAccount.getBalance()).toBe(expectedSecondBalance);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const balance = 88;
+
+    const spyRandom = jest.spyOn(lodash, 'random');
+    spyRandom.mockReturnValueOnce(balance);
+    spyRandom.mockReturnValueOnce(1);
+
+    const fetchedBalance = await mainAccount.fetchBalance();
+
+    expect(typeof fetchedBalance).toBe('number');
+    expect(fetchedBalance).toBe(balance);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const balance = 88;
+
+    const spyFetchBalance = jest.spyOn(mainAccount, 'fetchBalance');
+    spyFetchBalance.mockResolvedValueOnce(balance);
+
+    expect(mainAccount.getBalance()).toBe(initialBalance);
+    await mainAccount.synchronizeBalance();
+    expect(mainAccount.getBalance()).toBe(balance);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const spyFetchBalance = jest.spyOn(mainAccount, 'fetchBalance');
+    spyFetchBalance.mockResolvedValueOnce(null);
+
+    await expect(mainAccount.synchronizeBalance()).rejects.toThrowError(
+      SynchronizationFailedError,
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
