4b409a5dc74e9fadc44cbc88f10f66b2855116ad
EternalRival <eternalrival8@gmail.com>
2023-07-06 09:47:18 +0300

    refactor: change http-server to promise api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { httpServer } from './http_server/index';
+import { client } from './client';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+client.listen(HTTP_PORT);
