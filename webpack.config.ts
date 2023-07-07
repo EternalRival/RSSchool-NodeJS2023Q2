4be1fda371311679d3fd119081f5ab3f4d6cbb59
EternalRival <eternalrival8@gmail.com>
2023-07-07 17:18:43 +0300

    fix: add externals to fix ws errors

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,9 +4,10 @@ import ESLintPlugin from 'eslint-webpack-plugin';
 export default {
   mode: 'production',
   entry: resolve(__dirname, 'src', 'index'),
-  output: { clean: true, filename: 'index.js' },
+  output: { clean: true, filename: 'index.js', path: resolve(__dirname, 'dist', 'server') },
   module: { rules: [{ test: /\.ts$/i, loader: 'ts-loader' }] },
   resolve: { extensions: ['.ts', '.js'] },
   target: 'node',
+  externals: { 'bufferutil': 'bufferutil', 'utf-8-validate': 'utf-8-validate' },
   plugins: [new ESLintPlugin({ extensions: ['ts'], fix: false })],
 };
