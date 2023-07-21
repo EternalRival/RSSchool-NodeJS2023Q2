c14bed90d2b9bf400da3a39487e653b8abf68c95
EternalRival <eternalrival8@gmail.com>
2023-07-21 17:32:18 +0300

    feat: solve 2 tests

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,69 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { buildSchema, graphql } from 'graphql';
+import { PrismaClient } from '@prisma/client';
+import { UUIDType } from './types/uuid.js';
+
+const schema = buildSchema(`
+      enum MemberTypeId {
+        basic,
+        business
+      }
+
+      scalar UUID
+
+      type MemberType {
+        id: MemberTypeId
+        discount: Float
+        postsLimitPerMonth: Int
+      }
+
+      type Post {
+        id: UUID
+        title: String
+        content: String
+        authorId: UUID
+      }
+
+      type User {
+        id: UUID
+        name: String
+        balance: Float
+      }
+
+      type Profile {
+        id: UUID
+        isMale: Boolean
+        yearOfBirth: Int
+        memberTypeId: MemberTypeId
+        userId: UUID
+      }
+
+      type Query {
+        memberTypes: [MemberType]
+        memberType(id: MemberTypeId): MemberType
+        posts: [Post]
+        post(id: UUID): Post
+        users: [User]
+        user(id: UUID): User
+        profiles: [Profile]
+        profile(id: UUID): Profile
+      }
+
+    `);
+
+const prisma = new PrismaClient();
+
+const rootValue = {
+  memberTypes: () => prisma.memberType.findMany(),
+  memberType: ({ id }: { id: string }) => prisma.memberType.findUnique({ where: { id } }),
+  posts: () => prisma.post.findMany(),
+  post: ({ id }: { id: string }) => prisma.post.findUnique({ where: { id } }),
+  users: () => prisma.user.findMany(),
+  user: ({ id }: { id: string }) => prisma.user.findUnique({ where: { id } }),
+  profiles: () => prisma.profile.findMany(),
+  profile: ({ id }: { id: string }) => prisma.profile.findUnique({ where: { id } }),
+};
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +76,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+
+      const source = query;
+      const variableValues = variables;
+      console.log(query, variableValues);
+
+      const gql = await graphql({ schema, source, variableValues, rootValue });
+
+      return gql;
     },
   });
 };
