7affb64b0d7554665d11b200cff74cf6591c5d9f
EternalRival <eternalrival8@gmail.com>
2023-08-07 01:23:52 +0300

    feat: add typeorm to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,34 @@
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { User } from './users/entities/user.entity';
+import { Album } from './albums/entities/album.entity';
+import { Artist } from './artists/entities/artist.entity';
+import { Track } from './tracks/entities/track.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [User, Album, Artist, Track],
+        synchronize: true,
+      }),
+    }),
     UsersModule,
     TracksModule,
     ArtistsModule,
